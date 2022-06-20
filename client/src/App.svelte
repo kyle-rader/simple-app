641c4a86c4840c9688f0f32c2ea8d4d4861de4e7
Kyle Rader <kylewrader@gmail.com>
2022-06-19 20:52:37 -0700

    Setup TailwindCSS

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,65 +1,34 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import logo from "./assets/svelte.png";
+  import Counter from "./lib/Counter.svelte";
+
+  function toggle_darkmode() {
+    window.document.body.classList.toggle("dark");
+  }
 </script>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
+<img src={logo} alt="Svelte Logo" />
+<h1>Hello world!</h1>
 
-  <Counter />
+<button on:click={toggle_darkmode}>Toggle Dark Mode</button>
 
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
+<Counter />
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
-</main>
+<p>
+  Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+  apps.
+</p>
+
+<p>
+  Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for the
+  officially supported framework, also powered by Vite!
+</p>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
+  button {
+    display: block;
+    border: 2px;
+    border-color: aliceblue;
+    border-radius: 0.5em;
   }
 </style>
---
+++ client/tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{html,svelte,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
