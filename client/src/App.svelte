a0feaa334dc2e1d325a065092174db486b1aace4
Kyle Rader <kylewrader@gmail.com>
2022-06-30 14:27:39 -0700

    Render Router inside root

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -5,6 +5,7 @@
 </script>
 
 <main class:dark={$darkmode}>
-  <div id="root" class="bg-light0 text-dark0 dark:bg-dark0 dark:text-light0" />
-  <Router {routes} />
+  <div id="root" class="bg-light0 text-dark0 dark:bg-dark0 dark:text-light0">
+    <Router {routes} />
+  </div>
 </main>
