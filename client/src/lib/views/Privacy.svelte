fcc9abe8dce8f595c47cf4362582d2b61cd07d50
Kyle Rader <kylewrader@gmail.com>
2022-06-30 17:10:33 -0700

    center that text

--- client/src/lib/views/Privacy.svelte
+++ client/src/lib/views/Privacy.svelte
@@ -1,3 +1,7 @@
-<div class="container lg:px-48 md:px-24 sm:px-12 px-4 text-center">
-    <p class="text-3xl">Simple App collects zero information from you.</p>
+<div class="container mx-auto lg:px-48 md:px-24 sm:px-12 px-4">
+    <p class="text-3xl text-center">
+        Simple App collects zero information from you.
+        <br />
+        Period.
+    </p>
 </div>
