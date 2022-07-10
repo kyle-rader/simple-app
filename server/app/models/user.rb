ca44221005ca8aabfb1fa30b0ed6321f25f8f45a
Kyle Rader <kylewrader@gmail.com>
2022-07-10 13:34:46 -0700

    Remove User model

--- server/app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-    require "securerandom"
-
-    has_secure_password
-
-    validates :email, presence: true, uniqueness: true
-    validates :password, presence: true
-end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,7 +2,4 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "application#status"
-  resources :users
-  post '/auth/login', to: 'authentication#login'
-
 end
