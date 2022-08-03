36b5099bbffb85662a1be945966a48462134b7ed
Kyle Rader <kylewrader@gmail.com>
2022-08-02 21:00:52 -0700

    Make code css class

--- app/frontend/pages/main/index.svelte
+++ app/frontend/pages/main/index.svelte
@@ -6,8 +6,6 @@
   <h1 class="text-4xl mx-auto p-8">Welcome to Rails + Inertia + Svelte!</h1>
 
   <p>
-    You are logged in as <code class="rounded-md bg-dark0-soft px-2 py-1"
-      >{user}</code
-    >.
+    You are logged in as <code class="code">{user}</code>.
   </p>
 </div>
